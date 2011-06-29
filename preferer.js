d87c4a2edf83a26d4a067ad8822df160376b2331
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2011-06-29 16:34:09 +0000

    qunit setup done

---
+++ preferer.js
@@ -0,0 +1,4 @@
+var Ovi = Ovi || {};
+Ovi.Preferences = function(config){
+    
+};
--- test.js
+++ test.js
@@ -1,10 +1,23 @@
-module("setup test", {
+module("Preferer Tests", {
     setup: function() {
         this.User = { name: 'Jon' };
 		this.User.pref = new Ovi.Preferences();
 	}
 });
 
-test("expect in test", 1, function() {
-    ok(typeof this.User === "Object");
+test("setup test", 2, function() {
+    ok(typeof this.User === "object");
+    ok(typeof this.User.pref === "object");
+});
+
+test("preferences save test", 1, function() {
+    ok(false);
+});
+
+test("preferences load test", 1, function() {
+    ok(false);
+});
+
+test("preferences change test", 1, function() {
+    ok(false);
 });
